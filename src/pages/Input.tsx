511c7def2aa58e251d84b87eba64a713c0dbf1b7
QuangAnhTo <quangato072@gmail.com>
2026-03-15 22:39:16 +0700

    feat: refined padeye drawing components

--- src/pages/Input.tsx
+++ src/pages/Input.tsx
@@ -1,5 +1,11 @@
+import PadeyeDraft from "../components/konva/Padeye";
+
 export default function Input() {
   return (
-    <div className="text-3xl font-bold">Input</div>
+    <div className="text-3xl font-bold">Input
+    <div>
+        <PadeyeDraft/>
+    </div>
+    </div>
   )
 }
